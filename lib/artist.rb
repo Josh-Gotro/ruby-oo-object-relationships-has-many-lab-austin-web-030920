6fc3defdad379904f33da9cf791cfa020b6811e6
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-03-11 20:50:41 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,29 @@
 require 'pry'
 class Artist
-    
+    @@all = []
     attr_accessor :name
 
     def initialize (name)
         @name = name
-        @songs = []
+        @@all << self
     end
 
+    def songs
+    Song.all.select {|song| song.artist == self}
+    end
+    
+    def add_song (song)
+        # binding.pry
+       song.artist = self
+    end
+
+    def add_song_by_name (song_name)
+        song = Song.new(song_name)
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+      end
 
 end
